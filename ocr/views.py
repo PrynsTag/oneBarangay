a13af29e2d28c10ea3ff2a8435dace76937d27f7
PrynsTag <princevelasco16@gmail.com>
2021-08-29 20:21:11 +0800

    feat: Add Front-End OCR

--- ocr/views.py
+++ ocr/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
 # Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+
 def index(request):
     return HttpResponse("hello")
+
+
+def ocr(request):
+    context = {}
+    # Pick out the html file name from the url. And load that template.
+    load_template = request.path.split("/")[-1]
+    context["segment"] = load_template
+
+    html_template = loader.get_template(load_template)
+    return HttpResponse(html_template.render(context, request))
