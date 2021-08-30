1c2c07638024f04ee906e1cc83c599526a03654c
PrynsTag <princevelasco16@gmail.com>
2021-08-30 22:48:55 +0800

    refactor: Change URL Routing

--- ocr/urls.py
+++ ocr/urls.py
@@ -1,5 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
-urlpatterns = [path("", views.index)]
+urlpatterns = [
+    # The home page
+    path("ocr", views.index, name="ocr-home"),
+    re_path(r"^.*\.*", views.ocr, name="ocr-pages"),
+]
