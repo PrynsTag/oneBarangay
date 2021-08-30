fffcccdfa8a7d671392fa97096eea05e7cfa01c5
PrynsTag <princevelasco16@gmail.com>
2021-08-30 22:32:17 +0800

    chore: Remove Unnecessary File

--- app/admin-urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from ocr import views
-
-urlpatterns = [
-    # The home page
-    path("", views.index, name="home"),
-    path("ocr", views.ocr, name="ocr-views"),
-]
