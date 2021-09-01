c5bdb70e821dbe9e9b4fa3b5dd5cf050bc567eb0
Tricia Relox <201810394@fit.edu.ph>
2021-09-01 20:12:42 +0800

    add template

--- bulk_sched/views.py
+++ bulk_sched/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def add(request):
+    return render(request, "bulk_sched/add_bulk.html")
+
+
+# def add(response):
+#     return HttpResponse("HIII")
