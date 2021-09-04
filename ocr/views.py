6d6d6237ca53f12701e0a96338e917c3a71c82d1
PrynsTag <princevelasco16@gmail.com>
2021-09-04 19:19:59 +0800

    feat[form](BRGY-170): #comment create form validation and upload files
    Upload submitted files to GCS

--- ocr/views.py
+++ ocr/views.py
@@ -4,6 +4,48 @@ from django.shortcuts import render
 
 from ocr.models import File
 
+from .forms import FileFieldForm
+
+
+def file_upload(request):
+    """Handle file uploads.
+
+    Args:
+      request: The URL Request.
+
+    Returns:
+      On Success: Renders ocr_files to output uploaded files.
+      On File: Renders file_upload again to display validation errors
+    """
+    if request.method == "POST":
+        form = FileFieldForm(request.POST, request.FILES)
+        files = request.FILES.getlist("file_field")
+        if form.is_valid():
+            for file in files:
+                upload = File()
+                for chunks in file.chunks():
+                    upload.file_field.save(file.name, ContentFile(chunks))
+            return render(
+                request,
+                "ocr/ocr_files.html",
+                {"files": files},
+            )
+        else:
+            form = FileFieldForm()
+            return render(
+                request=request,
+                template_name="ocr/file_upload.html",
+                context={"form": form},
+            )
+    else:
+        form = FileFieldForm()
+        return render(
+            request=request,
+            template_name="ocr/file_upload.html",
+            context={"form": form},
+        )
+
+
 def ocr_files(request):
     """Fetch OCR HTML Pages.
 
