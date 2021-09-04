2aca3599111d8fd8cf60f4feadab250cc16ef11c
PrynsTag <princevelasco16@gmail.com>
2021-09-04 19:14:50 +0800

    feat[model](BRGY-170): #comment create model to upload image and file
    Uploads image and file to GCS

--- ocr/models.py
+++ ocr/models.py
@@ -1,1 +1,19 @@
 """Create your App models here."""
+from django.db import models
+
+from config.storage_backends import GoogleCloudMediaStorage
+
+
+class File(models.Model):
+    """Model to upload files and images in Google Cloud Storage."""
+
+    file_field = models.FileField(
+        upload_to="documents/", storage=GoogleCloudMediaStorage()
+    )
+    image_field = models.ImageField(
+        upload_to="images/", storage=GoogleCloudMediaStorage()
+    )
+
+    def __str__(self):
+        """Print File Class nicely."""
+        return f"{self.image_field.name}, ({self.image_field.size})"
