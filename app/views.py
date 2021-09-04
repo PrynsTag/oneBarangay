be1513b3ed85821723db94feaf5f57eb0ed2aebd
CWG.Ga <201812027@fit.edu.ph>
2021-09-04 20:44:12 +0800

    style: automatic code formatter

--- app/views.py
+++ app/views.py
@@ -26,7 +26,7 @@ def pages(request):
     Returns:
       The requested HTML File.
     """
-    # Pick out the html file name from the url. And load that template.
+    # Pick out the html file name from the url. And load that templates.
     # ex. barangay-admin/ocr/file-upload = file-upload
     load_template = request.path.split("/")[-1]
     html_template = loader.get_template(load_template)
