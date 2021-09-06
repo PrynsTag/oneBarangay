e36f3f34f02ef9665ea96fc8e6387afe1f07e5c7
PrynsTag <princevelasco16@gmail.com>
2021-09-06 23:15:25 +0800

    refactor[urls]: change function name

--- ocr/urls.py
+++ ocr/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path("upload", views.file_upload, name="upload"),
-    path("files", views.ocr_files, name="ocr_files"),
+    path("files", views.scan_file, name="files"),
     path("scan_file", views.scan_file, name="scan_file"),
 ]
