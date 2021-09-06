b1dc65d78f66e527d677106d6fd45b6828d9cf84
PrynsTag <princevelasco16@gmail.com>
2021-09-06 23:15:57 +0800

    refactor[urls]: add slug name to url

--- ocr/urls.py
+++ ocr/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path("upload", views.file_upload, name="upload"),
     path("files", views.scan_file, name="files"),
-    path("scan_file", views.scan_file, name="scan_file"),
+    path("scan_result/<str:name>", views.scan_result, name="scan_result"),
 ]
