1d86c348fb1a508aec29df4b8f371cd8205b6985
PrynsTag <princevelasco16@gmail.com>
2021-09-06 23:13:59 +0800

    refactor[model]: refactor variable name and add slug field

--- ocr/models.py
+++ ocr/models.py
@@ -9,16 +9,17 @@ from config.storage_backends import GoogleCloudMediaStorage
 class UploadFile(models.Model):
     """Model to upload files and images in Google Cloud Storage."""
 
-    file_field = models.FileField(
+    upload_file = models.FileField(
         upload_to="documents/", storage=GoogleCloudMediaStorage()
     )
-    image_field = models.ImageField(
+    slug = models.SlugField()
+    upload_image = models.ImageField(
         upload_to="images/", storage=GoogleCloudMediaStorage()
     )
 
     def __str__(self):
         """Print File Class nicely."""
-        return f"{self.image_field.name}, ({self.image_field.size})"
+        return f"{self.upload_file.name}"
 
     def get_absolute_url(self):
         """Get the absolute url."""
