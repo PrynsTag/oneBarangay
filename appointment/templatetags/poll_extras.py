7c4c109151e42b07e689b51a24a37033793fe4d2
John Christian Garon <201812027@fit.edu.ph>
2021-09-08 22:47:43 +0800

    refactor: added filters in templates (date and time formatter)

---
+++ appointment/templatetags/poll_extras.py
@@ -0,0 +1,63 @@
+from django import template
+import base64
+import datetime
+
+register = template.Library()
+
+
+@register.filter("base64_encode")
+def base64_encode(value):
+    """Convert text to base64.
+
+    Args:
+      value: Any string or texts
+
+    Returns: base64 string
+
+    """
+    value_bytes = value.encode("ascii")
+    base64_bytes = base64.urlsafe_b64encode(value_bytes)
+    base64_message = base64_bytes.decode("ascii")
+    return str(base64_message)
+
+
+@register.filter("date_formatter")
+def date_formatter(value):
+    """Format underscored date to MM DD, YYYY
+
+    Args:
+      value: Input date with underscore YYYY_MM_DD ("2021_09_08")
+
+    Returns: Standard date ("April 18, 2021")
+
+    """
+    split_date = value.split("_")
+    date = datetime.datetime(int(split_date[0]), int(split_date[1]), int(split_date[2]))
+
+    return date.strftime("%B %d, %Y")
+
+
+@register.filter("military_to_standard_time")
+def military_to_standard_time(value):
+    """Convert military time to standard time.
+
+    Args:
+      value: str: Input military time (ex. "0100", "0200", "0300")
+
+    Returns: standard time ("07:00 am")
+
+    """
+    hour_mod = int(value[:-2]) % 12
+    hour_two_dig = int(value[:-2])
+
+    if hour_two_dig % 12 == 0 and hour_two_dig == 0:
+        return f"12:00 am"
+    elif hour_two_dig % 12 == 0 and hour_two_dig == 12:
+        return f"12:00 pm"
+    elif hour_two_dig < 12:
+        return f"0{hour_mod}:00 am" if hour_mod < 10 else f"{hour_mod}:00 am"
+    elif hour_two_dig > 12:
+        return f"0{hour_mod}:00 pm" if hour_mod < 10 else f"{hour_mod}:00 pm"
+    else:
+        print("Invalid time please try again")
+        return "Invalid Time"
