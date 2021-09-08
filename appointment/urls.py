d6b8f330d68636013516fceac40c3cd0f5984dac
John Christian Garon <201812027@fit.edu.ph>
2021-09-08 22:48:25 +0800

    refactor: added url parameters for user's appointment details

--- appointment/urls.py
+++ appointment/urls.py
@@ -6,5 +6,10 @@ from . import views
 app_name = "appointment"
 
 urlpatterns = [
-    path("view-appointment", views.view_appointment, name="view-appointment")
+    path("view-appointment", views.view_appointment, name="view-appointment"),
+    path(
+        r"detail-appointment/<apt_details>",
+        views.details_appointment,
+        name="detail-appointment",
+    ),
 ]
