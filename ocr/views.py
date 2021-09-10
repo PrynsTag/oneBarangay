5bf6110045930cf4f05cf67edefdbcf6d108b3df
PrynsTag <princevelasco16@gmail.com>
2021-09-10 19:13:06 +0800

    feat(BRGY-217, BRGY-216, BRGY-207)[ocr]: #comment implement OCR #qa
    Implements scanning of document, scan result and edit result.

--- ocr/views.py
+++ ocr/views.py
@@ -1,13 +1,11 @@
 """Routing Request to Views of OCR Pages."""
-import json
-import os
-import re
+import asyncio
 
 from django.shortcuts import render
 from django.views.generic import FormView, ListView, TemplateView
 from dotenv import load_dotenv
-from google.cloud import storage, vision
 
+from ocr.form_recognizer import form_recognizer_runner
 from ocr.forms import UploadForm
 from ocr.models import Upload
 
@@ -69,94 +67,13 @@ class ScanResultView(TemplateView):
     template_name = "ocr/scan_result.html"
 
     def get_context_data(self, **kwargs):
-        """Get context data.
+        """Get context data and run form recognizer.
 
         Args:
-          **kwargs: Additional keyword argument.
+          **kwargs: Additional keyword argument (Filename).
 
         Returns:
-          scan_result.html with context of detected text from image.
+          scan_result.html with context of detected text from table image.
         """
-        gsc_source_uri = (
-            "gs://"
-            + os.getenv("GS_MEDIA_BUCKET_NAME")
-            + "/documents/"
-            + kwargs["filename"]
-        )
-        gcs_destination_uri = (
-            "gs://" + "scan_result" + "/documents/" + kwargs["filename"]
-        )
-        return {"ocr_text": async_detect_document(gsc_source_uri, gcs_destination_uri)}
-
-
-def async_detect_document(gcs_source_uri, gcs_destination_uri):
-    """OCR with PDF/TIFF as source files on GCS.
-
-    Args:
-      gcs_source_uri: Source URI of image.
-      gcs_destination_uri: Destination URI of the scanned result.
-
-    Returns:
-      The detected text from image.
-    """
-    # Supported mime_types are: 'application/pdf' and 'image/tiff'
-    mime_type = "image/tiff"
-
-    # How many pages should be grouped into each json output file.
-    batch_size = 2
-
-    client = vision.ImageAnnotatorClient()
-
-    feature = vision.Feature(type_=vision.Feature.Type.DOCUMENT_TEXT_DETECTION)
-
-    gcs_source = vision.GcsSource(uri=gcs_source_uri)
-    input_config = vision.InputConfig(gcs_source=gcs_source, mime_type=mime_type)
-
-    gcs_destination = vision.GcsDestination(uri=gcs_destination_uri)
-    output_config = vision.OutputConfig(
-        gcs_destination=gcs_destination, batch_size=batch_size
-    )
-
-    async_request = vision.AsyncAnnotateFileRequest(
-        features=[feature], input_config=input_config, output_config=output_config
-    )
-
-    operation = client.async_batch_annotate_files(requests=[async_request])
-
-    print("Waiting for the operation to finish.")
-    operation.result(timeout=420)
-
-    # Once the request has completed and the output has been
-    # written to GCS, we can list all the output files.
-    storage_client = storage.Client()
-
-    match = re.match(r"gs://([^/]+)/(.+)", gcs_destination_uri)
-    bucket_name = match.group(1)
-    prefix = match.group(2)
-
-    bucket = storage_client.get_bucket(bucket_name)
-
-    # List objects with the given prefix.
-    blob_list = list(bucket.list_blobs(prefix=prefix))
-    print("Output files:")
-    for blob in blob_list:
-        print(blob.name)
-
-    # Process the first output file from GCS.
-    # Since we specified batch_size=2, the first response contains
-    # the first two pages of the input file.
-    output = blob_list[0]
-
-    json_string = output.download_as_string()
-    response = json.loads(json_string)
-
-    # The actual response for the first page of the input file.
-    first_page_response = response["responses"][0]
-    annotation = first_page_response["fullTextAnnotation"]
-
-    # Here we print the full text from the first page.
-    # The response contains more information:
-    # annotation/pages/blocks/paragraphs/words/symbols
-    # including confidence scores and bounding boxes
-    print("Full text:\n")
-    return annotation["text"]
+        ocr = asyncio.run(form_recognizer_runner(kwargs["filename"]))
+        return {"ocr_header": ocr[0], "ocr_text": ocr[1]}
