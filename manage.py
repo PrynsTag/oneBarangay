01c5a865ceca911d7caa90def2885a807be0fe2e
PrynsTag <princevelasco16@gmail.com>
2021-09-11 13:26:52 +0800

    chore[rename]: rename oneBarangay to one_barangay
    chore[rename]: rename oneBarangay to one_barangay

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oneBarangay.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one_barangay.local_settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
