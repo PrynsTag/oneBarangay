d6ee3ed50b061ed04b35dd7f748f96270f08e047
PrynsTag <princevelasco16@gmail.com>
2021-09-11 13:55:43 +0800

    chore: remove unnecessary file

--- app/apps.py
+++
@@ -1,9 +0,0 @@
-"""This is a file for App Configuration."""
-from django.apps import AppConfig
-
-
-class AppConfig(AppConfig):
-    """A class for App Configuration."""
-
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app"
