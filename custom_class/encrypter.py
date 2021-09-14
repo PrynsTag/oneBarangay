0a56a8f0a53bfcd5a6ba4cb1804527dac6ad9501
PrynsTag <princevelasco16@gmail.com>
2021-09-14 18:28:01 +0800

    chore[docstring]: add docstring to modules and functions

--- custom_class/encrypter.py
+++ custom_class/encrypter.py
@@ -1,3 +1,4 @@
+"""Module for encrypting and decrypting text to base64."""
 import base64
 
 
@@ -5,13 +6,15 @@ class Encrypter:
     """This class encrypts and decrypts texts."""
 
     def __init__(self, text: str):
+        """Initialize text attribute."""
         self.text = text
 
     def __str__(self):
+        """Interpret class to text attribute."""
         return self.text
 
     def code_encoder(self):
-        """This class converts text to base64."""
+        """Encode text to base64."""
         text = self.text
         value_bytes = text.encode("ascii")
         base64_bytes = base64.urlsafe_b64encode(value_bytes)
@@ -19,7 +22,7 @@ class Encrypter:
         return str(base64_message)
 
     def code_decoder(self):
-        """This class converts text to base64."""
+        """Decode text to string."""
         value_bytes = self.text.encode("ascii")
         base64_bytes = base64.urlsafe_b64decode(value_bytes)
         base64_message = base64_bytes.decode("ascii")
