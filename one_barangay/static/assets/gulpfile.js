77dcdb7ad8ec00e1d7d25aaa5e5bd9158ea2882a
Restyled.io <commits@restyled.io>
2021-09-17 05:36:47 +0000

    Restyled by clang-format

--- one_barangay/static/assets/gulpfile.js
+++ one_barangay/static/assets/gulpfile.js
@@ -4,7 +4,8 @@
 * Volt Free - Bootstrap 5 Dashboard
 =========================================================
 
-* Product Page: https://themesberg.com/product/admin-dashboard/volt-premium-bootstrap-5-dashboard
+* Product Page:
+https://themesberg.com/product/admin-dashboard/volt-premium-bootstrap-5-dashboard
 * Copyright 2020 Themesberg (https://www.themesberg.com)
 * License (https://themesberg.com/licensing)
 
@@ -12,7 +13,9 @@
 
 =========================================================
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. Please contact us to request a removal.
+* The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software. Please contact us to request a
+removal.
 
 */
 
@@ -33,74 +36,68 @@ const rename = require('gulp-rename')
 // Define paths
 
 const paths = {
-  dist: {
-    base: './dist/',
-    css: './dist/css',
-    html: './dist/pages',
-    assets: './dist/assets',
-    img: './dist/assets/img',
-    vendor: './dist/vendor'
+  dist : {
+    base : './dist/',
+    css : './dist/css',
+    html : './dist/pages',
+    assets : './dist/assets',
+    img : './dist/assets/img',
+    vendor : './dist/vendor'
   },
-  dev: {
-    base: './html&css/',
-    css: './html&css/css',
-    html: './html&css/pages',
-    assets: './html&css/assets',
-    img: './html&css/assets/img',
-    vendor: './html&css/vendor'
+  dev : {
+    base : './html&css/',
+    css : './html&css/css',
+    html : './html&css/pages',
+    assets : './html&css/assets',
+    img : './html&css/assets/img',
+    vendor : './html&css/vendor'
   },
-  base: {
-    base: './',
-    node: './node_modules'
+  base : {base : './', node : './node_modules'},
+  src : {
+    base : './',
+    css : './css',
+    html : './src/pages/**/*.html',
+    assets : './src/assets/**/*.*',
+    partials : './src/partials/**/*.html',
+    scss : './scss',
+    node_modules : './node_modules/',
+    vendor : './vendor'
   },
-  src: {
-    base: './',
-    css: './css',
-    html: './src/pages/**/*.html',
-    assets: './src/assets/**/*.*',
-    partials: './src/partials/**/*.html',
-    scss: './scss',
-    node_modules: './node_modules/',
-    vendor: './vendor'
-  },
-  temp: {
-    base: './.temp/',
-    css: './.temp/css',
-    html: './.temp/pages',
-    assets: './.temp/assets',
-    vendor: './.temp/vendor'
+  temp : {
+    base : './.temp/',
+    css : './.temp/css',
+    html : './.temp/pages',
+    assets : './.temp/assets',
+    vendor : './.temp/vendor'
   }
 }
 
-// Compile SCSS
-gulp.task('scss', function () {
-  return gulp
-    .src([paths.src.scss + '/custom/**/*.scss', paths.src.scss + '/volt/**/*.scss', paths.src.scss + '/volt.scss'])
-    .pipe(wait(500))
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(
-      autoprefixer({
-        overrideBrowserslist: ['> 1%']
-      })
-    )
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.src.css))
-    .pipe(browserSync.stream())
-})
+              // Compile SCSS
+              gulp.task('scss', function() {
+                return gulp
+                    .src([
+                      paths.src.scss + '/custom/**/*.scss',
+                      paths.src.scss + '/volt/**/*.scss',
+                      paths.src.scss + '/volt.scss'
+                    ])
+                    .pipe(wait(500))
+                    .pipe(sourcemaps.init())
+                    .pipe(sass().on('error', sass.logError))
+                    .pipe(autoprefixer({overrideBrowserslist : [ '> 1%' ]}))
+                    .pipe(sourcemaps.write('.'))
+                    .pipe(gulp.dest(paths.src.css))
+                    .pipe(browserSync.stream())
+              })
 
 // Minify CSS
-gulp.task('minify:css', function () {
-  return gulp
-    .src([paths.src.css + '/volt.css'])
-    .pipe(cleanCss())
-    .pipe(
-      rename(function (path) {
+gulp.task('minify:css', function() {
+  return gulp.src([ paths.src.css + '/volt.css' ])
+      .pipe(cleanCss())
+      .pipe(rename(function(path) {
         // Updates the object in-place
         path.extname = '.min.css'
-      })
-    )
-    .pipe(gulp.dest(paths.src.css))
+      }))
+      .pipe(gulp.dest(paths.src.css))
 })
 
 // Default
