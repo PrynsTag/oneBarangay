6118c85a8fbbd2b59091a783e261042a90671683
PrynsTag <princevelasco16@gmail.com>
2021-09-17 12:17:23 +0800

    chore[settings]: change asgi to production settings

--- one_barangay/asgi.py
+++ one_barangay/asgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one_barangay.local_settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "one_barangay.settings")
 
 application = get_asgi_application()
