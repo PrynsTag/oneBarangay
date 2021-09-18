fb2f956e6672a6c4441c1ef27e808e0f29656100
CWG.Ga <201812027@fit.edu.ph>
2021-09-18 12:42:37 +0800

    fix: fixes local machine incorrect timezone

--- custom_class/dummy.py
+++ custom_class/dummy.py
@@ -5,7 +5,6 @@ from random import SystemRandom
 from faker import Faker
 from firebase_admin import auth, firestore
 from firebase_admin.exceptions import AlreadyExistsError
-from auth.service_account import firebase_authentication
 
 working_hours = [
     "1300",
@@ -31,6 +30,10 @@ document = [
 
 status = ["request", "in_progress", "get", "completed"]
 
+cryptogen = SystemRandom()
+fake = Faker()
+db = firestore.client()
+
 
 class Dummy:
     """Create Dummy accounts."""
@@ -42,16 +45,14 @@ class Dummy:
         Args:
           num_range: int:  Number of accounts
           password: str:  (Default value = "password123")
-
         Returns: add dummy accounts in firebase firestore
           num_range: int:
           password: str:
-        """
-        cryptogen = SystemRandom()
-        app = firebase_authentication()
-        db = firestore.client(app)
-        fake = Faker()
+          num_range: int:
+          password: str:
 
+        Returns: add accounts in firebase authentication and firestore
+        """
         for _ in range(0, num_range):
             first_name = fake.first_name()
             middle_name = fake.first_name()
@@ -87,69 +88,113 @@ class Dummy:
 
                 doc_ref.set(data, merge=True)
 
-    # Authentication Account and Firestore User Account with Appoinment
-    def create_dummy_appointment_with_account(self, password: str):
-        """Create dummy appointment with account in authentication and firestore.
+    def add_appointment_account(
+        self,
+        password: str = "password123",
+        year: int = datetime.date.today().year,
+        month: int = datetime.date.today().month,
+        day: int = datetime.date.today().day,
+        time_interval: int = 30,
+        time_start: int = 7,
+        time_end: int = 17,
+        utc_offset: int = 0,
+    ):
+        """For testing only.
 
         Args:
-            password: str:  default password 'password123'
+          password: str: input default password
+          year: int:  (Default value = None)
+          month: int:  (Default value = None)
+          day: int:  (Default value = None)
+          time_interval: int:  (Default value = None) appointment interval in minutes
+          time_start: int:  (Default value = None) start of appointment in 24hours (1 -> 01:00am)
+          time_end: int:  (Default value = None) end of appointment in 24hours (18 -> 06:00pm)
+          utc_offset: int:  (Default value = None) offset for utc
 
         Returns:
-            adds dummy accounts in firebase authentication and firebase firestore
+          : adds dummy accounts in firebase authentication and appointments in firebase firestore
         """
+        doc_ref_appointment = db.collection("appointments")
+        month = month if 1 > month > 12 else datetime.datetime.now().month
 
-        cryptogen = SystemRandom()
-        app = firebase_authentication()
-        db = firestore.client(app)
-        fake = Faker()
+        try:
+            datetime.datetime(year=year, month=month, day=day)
 
-        for a in range(700, 1701, 100):
-            first_name = fake.first_name()
-            middle_name = fake.first_name()
-            last_name = fake.last_name()
-            contact_no = fake.phone_number()
-            account_type = account_types[cryptogen.randrange(0, 3)]
-            email = fake.email(domain=None)
-            user_uid = None
-            sentence = fake.sentence(nb_words=10)
-            image = fake.file_name(category="image", extension="jpeg")
-            date = (datetime.datetime.now()).strftime("%Y_%m_%d")
-            time = f"0{str(a)}" if a < 1000 else str(a)
+        except ValueError:
+            day = datetime.datetime.today().day
 
-            doc_ref_account = db.collection("users").document("resident")
+        finally:
+            temp_hour = time_start
+            temp_minute = 0
 
-            try:
-                auth.create_user(email=email, password=password)
+            count = 1
 
-            except AlreadyExistsError:
-                print("Account already exist.")
+            while temp_hour < time_end:
+                print(f"Dummy Count: {count}")
+                count += 1
 
-            else:
-                user_uid = (auth.get_user_by_email(email=email)).uid
+                local_datetime = datetime.datetime.strptime(
+                    f"{year}-{month}-{day} {temp_hour}:{temp_minute}:{00}",
+                    "%Y-%m-%d %H:%M:%S",
+                )
 
-                data = {
-                    f"{user_uid}": {
+                result_utc = local_datetime - datetime.timedelta(hours=utc_offset)
+
+                # For end time of appointment
+                f_hour = temp_hour
+                f_minute = temp_minute
+
+                if (temp_minute + time_interval) == 60:
+                    f_hour += 1
+                    f_minute = 0
+                else:
+                    f_minute = f_minute + time_interval
+
+                local_datetime_adv = datetime.datetime.strptime(
+                    f"{year}-{month}-{day} {f_hour}:{f_minute}:{00}",
+                    "%Y-%m-%d %H:%M:%S",
+                )
+
+                result_utc_adv = local_datetime_adv - datetime.timedelta(hours=8)
+
+                first_name = fake.first_name()
+                middle_name = fake.first_name()
+                last_name = fake.last_name()
+                email = fake.email(domain=None)
+                contact_no = fake.phone_number()
+                account_type = "resident"
+                user_uid = None
+                sentence = fake.sentence(nb_words=10)
+                image = fake.file_name(category="image", extension="jpeg")
+
+                try:
+                    # Create Account in Firebase Authentication
+                    auth.create_user(email=email, password=password)
+
+                except AlreadyExistsError:
+                    print("Account already exist.")
+
+                else:
+                    # Get UID on created account in Firebase Authentication
+                    user_uid = auth.get_user_by_email(email=email).uid
+
+                    doc_ref_account = db.collection("users").document(user_uid)
+
+                    # Adding user accounts in firestore
+                    user_account_data = {
                         "first_name": first_name,
                         "middle_name": middle_name,
                         "last_name": last_name,
                         "email": email,
                         "contact_no": contact_no,
-                        "user_uid": f"{user_uid}",
                         "account_type": account_type,
+                        "created_on": datetime.datetime.now(),
                     }
-                }
 
-                doc_ref_account.set(data, merge=True)
+                    doc_ref_account.set(user_account_data, merge=True)
 
-                doc_ref_appointment = db.collection("appointments").document(
-                    str((datetime.datetime.now()).strftime("%Y_%m_%d"))
-                )
-
-                data = {
-                    (str(time) if a < 1000 else str(time)): {
-                        "appointment_id": "{date}_{time}".format(
-                            date=str(date), time=str(time)
-                        ),
+                    # Adding user appointments in firestore
+                    appointment_data = {
                         "first_name": first_name,
                         "middle_name": middle_name,
                         "last_name": last_name,
@@ -157,11 +202,18 @@ class Dummy:
                         "status": status[cryptogen.randrange(0, 4)],
                         "user_uid": user_uid,
                         "account_type": account_type,
-                        "appointment_date": date,
-                        "appointment_time": time,
+                        "start_appointment": result_utc,
+                        "end_appointment": result_utc_adv,
                         "appointment_purpose": sentence,
                         "appointment_image": image,
+                        "contact_no": contact_no,
+                        "created_on": datetime.datetime.now(),
                     }
-                }
 
-                doc_ref_appointment.set(data, merge=True)
+                    doc_ref_appointment.add(appointment_data)
+
+                    if (temp_minute + time_interval) == 60:
+                        temp_hour += 1
+                        temp_minute = 0
+                    else:
+                        temp_minute += time_interval
