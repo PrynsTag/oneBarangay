b5973d9d926a21c73b0eaf57c753ec2be1293c68
CWG.Ga <201812027@fit.edu.ph>
2021-09-18 12:43:35 +0800

    chore: added appointment searching in specific date

--- custom_class/firestore_data.py
+++ custom_class/firestore_data.py
@@ -70,3 +70,55 @@ class FirestoreData:
 
         for count, result in enumerate(result):
             print(f"Count: {count} {result.to_dict()}")
+
+    def day_appointments(
+        self, date: datetime.date = datetime.date.today(), utc_offset: int = 0
+    ):
+        """Get appointment date in firestore.
+
+        You can also manually search the appointment just input the date (year, month, day)
+
+        Args:
+          date: datetime.date:  (Default value = datetime.date.today())
+          utc_offset: int:  (Default value = 0)
+          date: datetime.date:  (Default value = datetime.date.today())
+          utc_offset: int:  (Default value = 0)
+
+        Returns:
+            : get appointment list in firebase firestore
+        """
+        import datetime
+
+        count = 1
+        appointment_list = []
+
+        year = int((str(date)).split(" ")[0].split("-")[0])
+        month = int((str(date)).split(" ")[0].split("-")[1])
+        day = int((str(date)).split(" ")[0].split("-")[2])
+
+        start_date = datetime.datetime.strptime(
+            f"{year}-{month}-{day - 1} {23}:{11}:{59}",
+            "%Y-%m-%d %H:%M:%S",
+        )
+
+        start_date_delta = start_date - datetime.timedelta(hours=utc_offset)
+
+        end_date = datetime.datetime.strptime(
+            f"{year}-{month}-{day} {23}:{11}:{59}", "%Y-%m-%d %H:%M:%S"
+        )
+
+        end_date_delta = end_date - datetime.timedelta(hours=utc_offset)
+
+        date_ref = (
+            db.collection("appointments")
+            .where("start_appointment", ">", start_date_delta)
+            .where("start_appointment", "<=", end_date_delta)
+            .order_by("start_appointment")
+        )
+        result = date_ref.get()
+
+        for count, appointment in enumerate(result):
+            count += 1
+            appointment_list.append(appointment.to_dict())
+
+        return count, appointment_list
