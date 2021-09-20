e98b4bc04aaac679c4c62cfff6e0eb1e0795caac
CWG.Ga <201812027@fit.edu.ph>
2021-09-20 22:29:38 +0800

    docs: format appointment template filters docstring

--- appointment/templatetags/poll_extras.py
+++ appointment/templatetags/poll_extras.py
@@ -14,7 +14,8 @@ def base64_encode(value):
     Args:
       value: Any string or texts
 
-    Returns: base64 string
+    Returns:
+        : base64 string
     """
     value_bytes = value.encode("ascii")
     base64_bytes = base64.urlsafe_b64encode(value_bytes)
@@ -29,7 +30,8 @@ def date_formatter(value):
     Args:
       value: Input date with underscore YYYY_MM_DD ("2021_09_08")
 
-    Returns: Standard date ("April 18, 2021")
+    Returns:
+        : Standard date ("April 18, 2021")
     """
     split_date = value.split("_")
     date = datetime.datetime(int(split_date[0]), int(split_date[1]), int(split_date[2]))
@@ -44,7 +46,8 @@ def military_to_standard_time(value):
     Args:
       value: str: Input military time (ex. "0100", "0200", "0300")
 
-    Returns: standard time ("07:00 am")
+    Returns:
+        : standard time ("07:00 am")
     """
     hour_mod = int(value[:-2]) % 12
     hour_two_dig = int(value[:-2])
@@ -58,7 +61,6 @@ def military_to_standard_time(value):
     elif hour_two_dig > 12:
         return f"0{hour_mod}:00 pm" if hour_mod < 10 else f"{hour_mod}:00 pm"
     else:
-        print("Invalid time please try again")
         return "Invalid Time"
 
 
