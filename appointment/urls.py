67a102cf9d53380f79a7b2b546228dac03521594
CWG.Ga <201812027@fit.edu.ph>
2021-09-20 22:30:16 +0800

    chore: added appointment url parameters

--- appointment/urls.py
+++ appointment/urls.py
@@ -6,10 +6,20 @@ from . import views
 app_name = "appointment"
 
 urlpatterns = [
-    path("view-appointment", views.view_appointment, name="view-appointment"),
     path(
-        r"detail-appointment/<apt_details>",
+        "view-appointment/<str:date>", views.view_appointment, name="view-appointment"
+    ),
+    path(
+        r"detail-appointment/<str:document_id>",
         views.details_appointment,
         name="detail-appointment",
     ),
+    path(
+        "id_verification/<str:document_id>",
+        views.id_verification,
+        name="id_verification",
+    ),
+    path("user_verified/<str:document_id>", views.user_verified, name="user_verified"),
+    path("add_appointment", views.add_appointment, name="test_appointment"),
+    path("delete_account", views.delete_account, name="delete_account"),
 ]
