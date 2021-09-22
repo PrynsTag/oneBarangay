147386c5b636ddd48bc773c4188dabe306983f8e
CWG.Ga <201812027@fit.edu.ph>
2021-09-22 13:27:50 +0800

    chore: update date and time formatter for firebase timestamp conversion

--- custom_class/dateformatter.py
+++ custom_class/dateformatter.py
@@ -1,5 +1,8 @@
 """Custom class date formatter."""
 import datetime
+import math
+
+from django.http import Http404
 
 
 class DateFormatter:
@@ -9,12 +12,16 @@ class DateFormatter:
         self,
         full_date: datetime.datetime = datetime.datetime.now(),
         date: datetime.datetime = datetime.date.today(),
+        datetime_str: str = "",
+        document_str="",
         separator: str = " ",
     ):
         """Initialize date formatter with full_date and separator."""
         self.full_date = full_date
         self.date = date
         self.separator = separator
+        self.datetime_str = datetime_str
+        self.document_str = document_str
 
     def date_splitter(self):
         """Split full date and time into date."""
@@ -25,7 +32,84 @@ class DateFormatter:
 
         return datetime.date(year=year, month=month, day=day)
 
+    # 2021-09-18 23:00:00+00:00
+    def datetime_splitter(self, utc_offset: int = 0):
+        datetime_split = str(self.full_date).split(" ")
+        date_split = datetime_split[0].split("-")
+        year = date_split[0]
+        month = date_split[1]
+        day = date_split[2]
+        time_split = datetime_split[1].split("+")[0].split(":")
+        hour = time_split[0]
+        minute = time_split[1]
+        second = time_split[2]
+
+        strp_datetime = datetime.datetime.strptime(
+            f"{year}-{month}-{day} {hour}:{minute}:{second}", "%Y-%m-%d %H:%M:%S"
+        )
+
+        result_utc = strp_datetime + datetime.timedelta(hours=utc_offset)
+
+        return result_utc
+
+    def document_splitter(self):
+        split_document_id = self.document_str.split("-")
+        get_date = split_document_id[0]
+        get_time = split_document_id[1]
+        year = int(get_date[:4])
+        month = int(get_date[4:6])
+        day = int(get_date[6:8])
+        time_int = int(get_time)
+
+        return get_date, get_time, year, month, day, time_int
+
+    def datetime_createdOn(self, utc_offset: int = 0):
+        datetime_split = str(self.datetime_str).split(" ")
+        date_split = datetime_split[0].split("-")
+        year = date_split[0]
+        month = date_split[1]
+        day = date_split[2]
+        time_split = datetime_split[1].split("+")[0].split(":")
+        hour = time_split[0]
+        minute = time_split[1]
+        second = str(int(time_split[2].split(".")[0]))
+
+        strp_datetime = datetime.datetime.strptime(
+            f"{year}-{month}-{day} {hour}:{minute}:{second}", "%Y-%m-%d %H:%M:%S"
+        )
+
+        result_utc = strp_datetime + datetime.timedelta(hours=utc_offset)
+
+        return result_utc
+
     def date_fb_convert(self):
         """Convert firebase DatetimeWithNanoseconds into python format date and time."""
         if isinstance(self.full_date, datetime.datetime):
             return self.full_date.__str__()
+
+    def firebaseTime_formatIt(self, utc_offset):
+        appointment_converted = self.date_fb_convert()
+        appointment_offset = self.firebase_utcOffset(
+            date=appointment_converted, utc_offset=utc_offset
+        )
+
+        return appointment_offset
+
+    def firebase_utcOffset(self, date: str, utc_offset: int = 0):
+        datetime_split = date.split(" ")
+        date_split = datetime_split[0].split("-")
+        year = date_split[0]
+        month = date_split[1]
+        day = date_split[2]
+        time_split = datetime_split[1].split("+")[0].split(":")
+        hour = time_split[0]
+        minute = time_split[1]
+        second = str(int(math.floor(float(time_split[2]))))
+
+        strp_datetime = datetime.datetime.strptime(
+            f"{year}-{month}-{day} {hour}:{minute}:{second}", "%Y-%m-%d %H:%M:%S"
+        )
+
+        result_utc = strp_datetime + datetime.timedelta(hours=utc_offset)
+
+        return result_utc
