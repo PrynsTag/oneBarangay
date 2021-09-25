08f455fae6498a1c1a5ddf87519f9aa04607493e
PrynsTag <princevelasco16@gmail.com>
2021-09-25 21:16:38 +0800

    refactor[runtime]: refactor main.py for running local and production

--- main.py
+++ main.py
@@ -3,18 +3,11 @@ import os
 
 import uvicorn
 
-from one_barangay import local_settings
 from one_barangay.asgi import application
 
 app = application
 
 if __name__ == "__main__":
-    os.environ["FIRESTORE_DATASET"] = "rbi"
-    os.environ["FIRESTORE_EMULATOR_HOST"] = "127.0.0.1:8080"
-    os.environ["FIRESTORE_EMULATOR_HOST_PATH"] = "127.0.0.1:8080/firestore"
-    os.environ["FIRESTORE_HOST"] = "http://127.0.0.1:8080"
-    os.environ["FIRESTORE_PROJECT_ID"] = "onebarangay-malanday"
-
     if os.getenv("GAE_ENV", "").startswith("standard"):
         # production
         uvicorn.run(
@@ -24,18 +17,11 @@ if __name__ == "__main__":
             workers=1,
         )
     else:
-        local_settings.STATICFILES_STORAGE = (
-            "storages.backends.gcloud.GoogleCloudStorage"
-        )
-        local_settings.DEFAULT_FILE_STORAGE = (
-            "django.core.files.storage.FileSystemStorage"
-        )
-        local_settings.STATIC_URL = "https://storage.googleapis.com/{}/".format(
-            local_settings.GS_STATIC_BUCKET_NAME
-        )
-        local_settings.MEDIA_URL = "https://storage.googleapis.com/{}/".format(
-            local_settings.GS_MEDIA_BUCKET_NAME
-        )
+        os.environ["FIRESTORE_DATASET"] = "rbi"
+        os.environ["FIRESTORE_EMULATOR_HOST"] = "127.0.0.1:8080"
+        os.environ["FIRESTORE_EMULATOR_HOST_PATH"] = "127.0.0.1:8080/firestore"
+        os.environ["FIRESTORE_HOST"] = "http://127.0.0.1:8080"
+        os.environ["FIRESTORE_PROJECT_ID"] = "onebarangay-malanday"
 
         uvicorn.run(
             "main:app",
