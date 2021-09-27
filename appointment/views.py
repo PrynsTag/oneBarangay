d842609e9087aa8456695edabbf327f89488c775
CWG.Ga <201812027@fit.edu.ph>
2021-09-27 23:05:54 +0800

    chore: added appointment resched and id verification

--- appointment/views.py
+++ appointment/views.py
@@ -22,7 +22,7 @@ def view_appointment(request, date):
       date: appointment date
 
     Returns:
-      : The view_appointment template and the appointments and working hours context data.
+      The view_appointment template and the appointments and working hours context data.
     """
     for key in list(request.session.keys()):
         del request.session[key]
@@ -73,7 +73,7 @@ def view_appointment(request, date):
         )
 
 
-def details_appointment(request, document_id):
+def request(request, document_id):
     """Display the details of the user's appointment.
 
     Args:
@@ -81,7 +81,7 @@ def details_appointment(request, document_id):
       document_id: user appointment document ID
 
     Returns:
-      : renders the html of appointment details
+      renders the html of appointment details
     """
     form = IdVerification()
     encrypter = Encrypter(text=document_id).code_decoder()
@@ -115,57 +115,124 @@ def details_appointment(request, document_id):
                 full_date=appointment_detail["created_on"]
             ).firebaseTime_formatIt(utc_offset=8)
 
-            print(f"Start appointment detail formatted: {start_appointment_formatted}")
-            print(f"End appointment detail formatted: {end_appointment_formatted}")
-            print(
-                f"Created On appointment detail formatted: {createdOn_appointment_formatted}"
-            )
+            appointment_detail["start_appointment"] = start_appointment_formatted
+            appointment_detail["end_appointment"] = end_appointment_formatted
+            appointment_detail["created_on"] = createdOn_appointment_formatted
 
-            if "user_verified" in request.session:
+            reschedule = (
+                DateFormatter().documentid_to_datetime(document_id=encrypter)
+            ).date()
 
-                request.session["user_info"] = appointment_detail
+            if "user_check" in request.session and request.session["user_check"]:
+                user_list = request.session["user_list"]
+                user_verified_session = request.session["user_verified"]
 
-                id_encode = Encrypter(text=document_id).code_encoder()
+                if user_verified_session:
+                    return render(
+                        request,
+                        "appointment/details_appointment.html",
+                        {
+                            "reschedule": reschedule,
+                            "user_check": True,
+                            "user_verified": True,
+                            "user_detail": appointment_detail,
+                            "user_list": user_list,
+                            "amount": 100,
+                            "form": form,
+                            "back": str(datetime.date(year=year, month=month, day=day)),
+                            "document_id": document_id,
+                        },
+                    )
+                else:
 
-                return render(
-                    request,
-                    "appointment/details_appointment.html",
-                    {
-                        "user_verified": True,
-                        "user_detail": appointment_detail,
-                        "amount": 100,
-                        "form": form,
-                        "back": str(datetime.date(year=year, month=month, day=day)),
-                        "document_id": id_encode,
-                    },
-                )
+                    return render(
+                        request,
+                        "appointment/details_appointment.html",
+                        {
+                            "reschedule": reschedule,
+                            "user_check": True,
+                            "user_verified": False,
+                            "user_detail": appointment_detail,
+                            "user_list": user_list,
+                            "amount": 100,
+                            "form": form,
+                            "back": str(datetime.date(year=year, month=month, day=day)),
+                        },
+                    )
             else:
+
                 return render(
                     request,
                     "appointment/details_appointment.html",
                     {
-                        "user_verified": False,
+                        "reschedule": reschedule,
                         "user_detail": appointment_detail,
                         "amount": 100,
                         "form": form,
                         "back": str(datetime.date(year=year, month=month, day=day)),
+                        "user_check": False,
                     },
                 )
-        else:
-            raise Http404("Invalid Input")
+
+
+def appointment_resched(request, document_id, date):
+    """Get appointment lists for reschedule.
+
+    Args:
+      request: The URL request.
+      document_id: document id in firebase firestore
+      date: for list of appointments on a specific date
+
+    Returns:
+        list of appointments
+    """
+    encrypter = Encrypter(text=document_id).code_decoder()
+    current_date = datetime.datetime.strptime(date, "%Y-%m-%d")
+
+    resched_list = firestoreQuery.resched_appointment(
+        year=current_date.year,
+        month=current_date.month,
+        day=current_date.day,
+        hour=23,
+        minute=59,
+        second=59,
+        document_id=encrypter,
+        utc_offset=8,
+        datetime=datetime,
+        query_list=["start_appointment", "end_appointment", "created_on"],
+    )
+
+    next = current_date + datetime.timedelta(days=1)
+    previous = current_date - datetime.timedelta(days=1)
+    current = (DateFormatter().documentid_to_datetime(document_id=encrypter)).date()
+    today = datetime.date.today()
+
+    return render(
+        request,
+        "appointment/reschedule.html",
+        {
+            "current_date": current_date.strftime("%B %d, %Y"),
+            "appointment_list": resched_list,
+            "next": next.date(),
+            "previous": previous.date(),
+            "current": current,
+            "today": today,
+            "document_id": document_id,
+            "back": document_id,
+        },
+    )
 
 
 def id_verification(request, document_id):
     """Check user ID for verification.
 
     Args:
-      request: Returns: view of appointment details.
+      request: The URL request.
       document_id: user appointment document ID
 
     Returns:
-        : change document status
+      change document status
     """
-    verify_user = FirestoreData()
     document_id_decrypt = Encrypter(text=document_id).code_decoder()
 
     if request.method == "POST":
@@ -176,31 +243,47 @@ def id_verification(request, document_id):
             field_middlename = verification_form.cleaned_data.get("middle_name")
             field_lastname = verification_form.cleaned_data.get("last_name")
 
-            results = verify_user.verify_identification(
+            # Get results
+            results = firestoreQuery.verify_identification(
                 firstname=field_firstname,
                 middlename=field_middlename,
                 lastname=field_lastname,
             )
 
-            convert_fb_timestamp = DateFormatter(
-                full_date=results[0]["created_on"]
-            ).date_fb_convert()
+            user_results = []
 
-            results[0]["created_on"] = convert_fb_timestamp
+            for result in results:
+                result["created_on"] = DateFormatter(
+                    full_date=result["created_on"]
+                ).date_fb_convert()
+                user_results.append(result)
 
-            if len(results) == 1:
+            if len(user_results) == 1:
+                request.session["user_check"] = True
                 request.session["user_verified"] = True
-                request.session["user_info"] = results[0]
+                request.session["user_list"] = user_results
                 request.session["document_id"] = document_id_decrypt
 
                 return HttpResponseRedirect(
                     reverse(
-                        "appointment:detail-appointment",
+                        "appointment:process",
+                        kwargs={"document_id": document_id},
+                    )
+                )
+            elif len(user_results) > 1:
+                request.session["user_check"] = True
+                request.session["user_verified"] = False
+                request.session["document_id"] = document_id_decrypt
+                request.session["user_list"] = user_results
+
+                return HttpResponseRedirect(
+                    reverse(
+                        "appointment:process",
                         kwargs={"document_id": document_id},
                     )
                 )
             else:
-                return HttpResponse("Duplicate user info")
+                raise Http404("Data not found.")
         else:
             return HttpResponse("Invalid input.")
     else:
@@ -210,6 +293,75 @@ def id_verification(request, document_id):
         )
 
 
+def id_verification_manual(request, user_uid):
+    """Verify ID of resident if the account exist.
+
+    Args:
+      request: The URL request.
+      user_uid: user id in firebase firestore
+
+    Returns:
+        session indicating that the user was verified
+    """
+    search_appointment = [firestoreQuery.search_appointment_userId(user_uid=user_uid)]
+
+    user_list = []
+
+    for user in list(search_appointment):
+        user["start_appointment"] = DateFormatter(
+            full_date=user["start_appointment"]
+        ).date_fb_convert()
+        user["end_appointment"] = DateFormatter(
+            full_date=user["end_appointment"]
+        ).date_fb_convert()
+        user["created_on"] = DateFormatter(
+            full_date=user["created_on"]
+        ).date_fb_convert()
+        user_list.append(user)
+
+    request.session["user_check"] = True
+    request.session["user_verified"] = True
+    request.session["user_list"] = user_list
+    request.session["document_id"] = search_appointment[0]["document_id"]
+
+    return HttpResponseRedirect(
+        reverse(
+            "appointment:process",
+            kwargs={
+                "document_id": Encrypter(
+                    text=search_appointment[0]["document_id"]
+                ).code_encoder()
+            },
+        )
+    )
+
+
+def process(request, document_id):
+    """Process documents of resident.
+
+    Args:
+      request: The URL request.
+      document_id: document id of appointment
+
+    Returns:
+        document
+    """
+    return HttpResponse(f"Status: Process, Document ID: {document_id}")
+
+
+def get(request, document_id):
+    """Notify resident if document is already completed.
+
+    Args:
+      request: The URL request.
+      document_id: document id of appointment collection in firebase firestore
+
+    Returns:
+        : sending notification to resident
+    """
+    return HttpResponse(f"Status: Get, Document ID: {document_id}")
+
+
 def add_appointment(request):
     """For date testing only.
 
@@ -217,10 +369,10 @@ def add_appointment(request):
       request: Returns: add date in firestore
 
     Returns:
-        : add account in firebase authentication and firestore
+      add account in firebase authentication and firestore
     """
     firestore_add_date = Dummy()
-    firestore_add_date.add_appointment_account(time_interval=15, utc_offset=8, day=21)
+    firestore_add_date.add_appointment_account(time_interval=15, utc_offset=8)
 
     return HttpResponseRedirect(reverse("services:index"))
 
@@ -232,7 +384,7 @@ def delete_account(request):
       request: Returns: delete accounts.
 
     Returns:
-        : delete accounts in firebase authentication and firestore
+      delete accounts in firebase authentication and firestore
     """
     firestoreQuery.delete_account_auth()
 
@@ -247,15 +399,25 @@ def user_verified(request, document_id):
       document_id: user appointment document ID
 
     Returns:
-        : Change user's document status
+      Change user's document status
     """
     document_id_decode = Encrypter(text=document_id).code_decoder()
 
     if "user_verified" in request.session and "document_id" in request.session:
+
         session_document_id = request.session["document_id"]
+
         if session_document_id == document_id_decode:
-            print("session document id and document id are the same")
+            # user_info = request.session["user_list"]
+
+            # user_uid = user_info[0]["user_uid"]
+
+            print(
+                firestoreQuery.search_document(
+                    document_id=Encrypter(text=document_id_decode).code_encoder()
+                )
+            )
+
+            return HttpResponse("User Verified")
         else:
             print("session document id and document are not the same")
-
-    raise Http404("In user verified")
