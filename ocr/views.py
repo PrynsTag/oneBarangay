b5731a7cd6fd776b63622de6c1086cfa420a3754
PrynsTag <princevelasco16@gmail.com>
2021-09-28 03:49:13 +0800

    feat(BRGY-174)[OCR]: #comment implement OCR #done
    BRGY-323 - #comment implement dropzone.js
    BRGY-207 - #comment implement scan document
    BRGY-322 BRGY-170 - #comment file upload using default_storage
    BRGY-173 - #comment implement fully-featured bootstrap tables

--- ocr/views.py
+++ ocr/views.py
@@ -1,15 +1,21 @@
 """Routing Request to Views of OCR Pages."""
 import asyncio
+import json
+import logging
 
 from django.core.files.storage import default_storage
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import FormView, TemplateView
 from dotenv import load_dotenv
 
 from ocr.form_recognizer import form_recognizer_runner
 from ocr.forms import UploadForm
+from ocr.scripts import Script
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 
 class FileUploadView(FormView):
@@ -18,6 +24,10 @@ class FileUploadView(FormView):
     form_class = UploadForm
     template_name = "ocr/file_upload.html"
     success_url = "ocr/ocr_files.html"
+
+    def __init__(self):
+        """Initialize FileUploadView class variables."""
+        self.script = Script()
 
     def post(self, request, *args, **kwargs):
         """Post request from file upload.
@@ -31,26 +41,53 @@ class FileUploadView(FormView):
           on success: The ocr_files along with context.
           on fail: The file_upload.html along with context.
         """
-        form = UploadForm(request.POST, request.FILES)
-        files = request.FILES.getlist("upload_file")
+        files = [request.FILES[file] for file in request.FILES]
+        # TODO: Get thumbnail data from dropzone
+        # TODO: Pass the thumbnail data to session
+        dict_files = [json.loads(file) for file in request.POST.getlist("fileData")]
 
-        if form.is_valid():
-            for file in files:
-                default_storage.save(file.name, file)
+        request.session["files"] = self.script.format_dictionary_file(dict_files)
 
-            return render(
-                request,
-                "ocr/ocr_files.html",
-                {"files": files},
-            )
-        else:
-            form = UploadForm()
+        for file in files:
+            default_storage.save(file.name, file)
 
-            return render(
-                request,
-                "ocr/file_upload.html",
-                {"form": form},
-            )
+        return HttpResponse(request.session["files"])
+
+
+class ScanFileView(FormView):
+    """View for file upload."""
+
+    template_name = "ocr/file_upload.html"
+
+    def post(self, request, *args, **kwargs):
+        """Get context data and run form recognizer.
+
+        Args:
+          request: The URL Request.
+          **kwargs: Additional keyword argument (Filename).
+          *args: Additional Arguments
+
+        Returns:
+          : scan_result.html with context of detected text from table image.
+        """
+        return render(request, self.template_name)
+
+
+class OCRFilesView(TemplateView):
+    """Display ocr_files template."""
+
+    template_name = "ocr/ocr_files.html"
+
+    def get_context_data(self, **kwargs):
+        """Get context data of ocr_files.
+
+        Args:
+          **kwargs: Keyword arguments.
+
+        Returns:
+          : files as context data.
+        """
+        return {"files": self.request.session["files"]}
 
 
 class ScanResultView(TemplateView):
