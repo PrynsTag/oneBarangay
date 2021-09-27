21701fcdcbb58e477e5530c990ad5d1152156998
CWG.Ga <201812027@fit.edu.ph>
2021-09-27 23:04:13 +0800

    chore: added url path

--- appointment/urls.py
+++ appointment/urls.py
@@ -11,8 +11,8 @@ urlpatterns = [
     ),
     path(
         r"detail-appointment/<str:document_id>",
-        views.details_appointment,
-        name="detail-appointment",
+        views.request,
+        name="request",
     ),
     path(
         "id_verification/<str:document_id>",
@@ -20,6 +20,18 @@ urlpatterns = [
         name="id_verification",
     ),
     path("user_verified/<str:document_id>", views.user_verified, name="user_verified"),
+    path(
+        "id_verification_manual/<str:user_uid>",
+        views.id_verification_manual,
+        name="id_verification_manual",
+    ),
+    path(
+        "appointment_resched/<str:document_id>/<str:date>",
+        views.appointment_resched,
+        name="appointment_resched",
+    ),
+    path("process/<str:document_id>", views.process, name="process"),
+    path("get/<str:document_id>", views.get, name="get"),
     path("add_appointment", views.add_appointment, name="test_appointment"),
     path("delete_account", views.delete_account, name="delete_account"),
 ]
