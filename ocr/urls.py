de9a7531149cf26439bcdd024af7a3f6657ec246
PrynsTag <princevelasco16@gmail.com>
2021-09-28 04:00:17 +0800

    chore[OCR]: add urls for ocr

--- ocr/urls.py
+++ ocr/urls.py
@@ -5,9 +5,19 @@ from . import views
 
 urlpatterns = [
     path("upload", views.FileUploadView.as_view(), name="upload"),
+    path("files", views.ScanFileView.as_view(), name="files"),
+    path("ocr_files", views.OCRFilesView.as_view(), name="ocr_files"),
     path(
         "scan_result/<str:filename>",
         views.ScanResultView.as_view(),
         name="scan_result",
+    ),
+    path("save_result", views.SaveScanResultView.as_view(), name="save_result"),
+    path("", views.RBITableView.as_view(), name="rbi_table"),
+    path("rbi", views.RBIView.as_view(), name="rbi"),
+    path(
+        "<str:page>/<str:created_at>/",
+        views.RBIView.as_view(),
+        name="rbi",
     ),
 ]
