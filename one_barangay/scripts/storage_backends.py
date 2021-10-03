5390e7a5abdcd1b824e5d93cea7dca358083fb99
PrynsTag <princevelasco16@gmail.com>
2021-10-04 04:32:22 +0800

    chore[aws]: remove aws from storage_backends

--- one_barangay/scripts/storage_backends.py
+++ one_barangay/scripts/storage_backends.py
@@ -2,17 +2,12 @@
 import json
 import logging
 import os
-import sys
-import threading
 from abc import ABC
 from json import JSONDecodeError
 from urllib.parse import urljoin
 
-import boto3
 from azure.core.exceptions import ResourceExistsError
 from azure.storage.blob import BlobClient, ContentSettings
-from boto3.exceptions import S3UploadFailedError
-from botocore.exceptions import ClientError
 from django.conf import settings
 from dotenv import load_dotenv
 from storages.backends.gcloud import GoogleCloudStorage
@@ -79,110 +74,6 @@ class GoogleCloudStaticStorage(GoogleCloudStorage, ABC):
           The URL path.
         """
         return urljoin(settings.STATIC_URL, name)
-
-
-class ProgressPercentage:
-    """Class for progress bar UI."""
-
-    def __init__(self, filename):
-        """Initialize file properties."""
-        self._filename = filename
-        self._size = float(os.path.getsize(filename))
-        self._seen_so_far = 0
-        self._lock = threading.Lock()
-
-    def __call__(self, bytes_amount):
-        """Call for displaying progress."""
-        # To simplify, assume this is hooked up to a single filename
-        with self._lock:
-            self._seen_so_far += bytes_amount
-            percentage = (self._seen_so_far / self._size) * 100
-            sys.stdout.write(
-                f"\r{self._filename}  {self._seen_so_far} / {self._size}  {percentage:.2f}"
-            )
-            sys.stdout.flush()
-
-
-class AwsS3:
-    """Class to interact with AWS."""
-
-    def __init__(self, bucket_name):
-        """Initialize AWS properties and credentials."""
-        self.bucket_name = bucket_name
-        self.s3_client = boto3.client(
-            "s3",
-            aws_access_key_id=os.getenv("aws_access_key_id"),
-            aws_secret_access_key=os.getenv("aws_secret_access_key"),
-            aws_session_token=os.getenv("aws_session_token"),
-        )
-        self.s3_resource = boto3.resource(
-            "s3",
-            aws_access_key_id=os.getenv("aws_access_key_id"),
-            aws_secret_access_key=os.getenv("aws_secret_access_key"),
-            aws_session_token=os.getenv("aws_session_token"),
-        ).Bucket(bucket_name)
-
-    def upload_local_json_file(self, file_name, canonical_id, object_name=None):
-        """Upload local json file to AWS.
-
-        Args:
-          file_name: The name of a file to upload.
-          canonical_id: The canonical id of a user.
-          object_name: (Default value = None) The object name of a file.
-
-        Returns:
-          None.
-        """
-        # If S3 object_name was not specified, use file_name
-        if object_name is None:
-            object_name = os.path.basename(file_name)
-
-        # Upload the file
-        try:
-            self.s3_client.upload_file(
-                file_name,
-                self.bucket_name,
-                object_name,
-                Callback=ProgressPercentage(file_name),
-                ExtraArgs={
-                    "ContentType": "application/json",
-                    "GrantRead": "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-                    "GrantFullControl": f"id={canonical_id}",
-                },
-            )
-        except ClientError as e:
-            logger.error(e)
-        except S3UploadFailedError as e:
-            logger.error(e)
-
-    def append_to_local_json_file(self, key, canonical_id, new_row):
-        """Append row to local json file.
-
-        Args:
-          key: The key or filename of a file.
-          canonical_id: Canonical ID of a user.
-          new_row: The new row to append to.
-
-        Returns:
-          None.
-        """
-        s3_object = self.s3_resource.Object(key=key)
-
-        json_data = json.load(s3_object.get()["Body"])
-
-        json_data["rows"] += new_row
-        json_data["total"] = len(json_data["rows"])
-        json_data["totalNotFiltered"] = json_data["total"]
-
-        with open("rbi_data.json", "w", encoding="UTF-8") as file:
-            json.dump(json_data, file)
-
-        s3_object.put(
-            Body=json.dumps(json_data),
-            ContentType="application/json",
-            GrantRead="uri=http://acs.amazonaws.com/groups/global/AllUsers",
-            GrantFullControl=f"id={canonical_id}",
-        )
 
 
 class AzureStorageBlob:
