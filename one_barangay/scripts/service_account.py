1d31edc00fbed4415143b4fc107cc4b98cd972d6
PrynsTag <princevelasco16@gmail.com>
2021-10-05 00:20:26 +0800

    refactor[service_account]: add base64 function and gcloud authentication

--- one_barangay/scripts/service_account.py
+++ one_barangay/scripts/service_account.py
@@ -1,35 +1,53 @@
 """Authenticate Google and Firebase Service Accounts."""
+import ast
 import base64
-import json
 import logging
 import os
 
 import firebase_admin
 from dotenv import load_dotenv
 from firebase_admin import credentials
+from google.oauth2 import service_account
 
 load_dotenv()
 
 logger = logging.getLogger(__name__)
 
 
-def firestore_auth(name="firestore_app", b64_env_name="GOOGLE_STORAGE_CREDENTIALS"):
-    """Authenticate to cloud firestore using base64 decoder.
+def decode_b64_to_dict(b64_env_name="GOOGLE_STORAGE_CREDENTIALS"):
+    """Decode base64 value to dictionary.
 
     Args:
       b64_env_name:
-        (Default value = "GOOGLE_STORAGE_CREDENTIALS") A environment variable with base64 value.
+        (Default value = "GOOGLE_STORAGE_CREDENTIALS") The value to decode.
+
+    Returns:
+      The dictionary credential or None if exception occured.
+    """
+    try:
+        logging.info("Decoding started.")
+
+        decoded_bytes = base64.b64decode(os.getenv(b64_env_name))
+        decoded_str = str(decoded_bytes, "utf-8")
+        dictionary_credential = ast.literal_eval(decoded_str)
+
+        logging.info("Decoding successful.")
+        return dictionary_credential
+    except ValueError as e:
+        logger.exception("Decoding failed. %s", e)
+
+
+def firestore_auth(name="firestore_app"):
+    """Authenticate to cloud firestore using base64 decoder.
+
+    Args:
       name: (Default value = "firestore_app") The name of the app if called multiple times.
 
     Returns:
       A newly initialized instance of App.
     """
     try:
-        decoded_bytes = base64.b64decode(os.getenv(b64_env_name))
-        decoded_str = str(decoded_bytes, "utf-8")
-        dictionary_credential = json.loads(decoded_str)
-
-        cred = credentials.Certificate(dictionary_credential)
+        cred = credentials.Certificate(decode_b64_to_dict())
         logger.info("creating credential...")
 
         app = firebase_admin.initialize_app(cred, name=name)
@@ -38,3 +56,14 @@ def firestore_auth(name="firestore_app", b64_env_name="GOOGLE_STORAGE_CREDENTIAL
         return app
     except ValueError as e:
         logger.exception("Credential creation failed. %s", e)
+
+
+def gcloud_auth():
+    """Authenticate to Google Cloud."""
+    try:
+        dictionary_credential = service_account.Credentials.from_service_account_info(
+            decode_b64_to_dict()
+        )
+        return dictionary_credential
+    except ValueError as e:
+        logger.exception("Decoding failed. %s", e)
