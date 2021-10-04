9b3c003409c91ac69ea21ffa2a099facef308196
PrynsTag <princevelasco16@gmail.com>
2021-10-04 17:49:01 +0800

    fix[service_account]: use base64 value as credential

--- one_barangay/scripts/service_account.py
+++ one_barangay/scripts/service_account.py
@@ -1,7 +1,8 @@
 """Authenticate Google and Firebase Service Accounts."""
+import base64
+import json
 import logging
 import os
-import re
 
 import firebase_admin
 from dotenv import load_dotenv
@@ -12,18 +13,26 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 
 
-def firestore_auth(name="firestore_app"):
-    """Authenticate to cloud firestore."""
+def firestore_auth(b64_env_name="GOOGLE_STORAGE_CREDENTIALS", name="firestore_app"):
+    """Authenticate to cloud firestore using base64 decoder.
+
+    Args:
+      b64_env_name: (Default value = "GOOGLE_STORAGE_CREDENTIALS")
+      name: (Default value = "firestore_app") The name of the app if called multiple times.
+
+    Returns:
+      A newly initialized instance of App.
+    Raises:
+      ValueError -  If any of the provided arguments are invalid.
+    """
     try:
-        dict_credential = {
-            "type": "service_account",
-            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-            "private_key": re.sub(r"/\\n/g", "\n", os.getenv("FIREBASE_PRIVATE_KEY")),
-            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        }
-        cred = credentials.Certificate(dict_credential)
+        decoded_bytes = base64.b64decode(os.getenv(b64_env_name))
+        decoded_str = str(decoded_bytes, "utf-8")
+        dictionary_credential = json.loads(decoded_str)
+
+        cred = credentials.Certificate(dictionary_credential)
         logger.info("creating credential...")
+
         app = firebase_admin.initialize_app(cred, name=name)
         logger.info("Credentials successfully created..")
 
