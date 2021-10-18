f889bfb7f8c5e3b2fe445ba1f45d10436df30de4
PrynsTag <princevelasco16@gmail.com>
2021-10-19 07:24:20 +0800

    chore[ocr]: add function for getting resident's rbi

--- ocr/firestore_model.py
+++ ocr/firestore_model.py
@@ -4,9 +4,11 @@ import os
 
 import requests
 from firebase_admin import firestore
+from google.api_core.exceptions import NotFound
 
 from ocr.dummy_data import RBIDummy
 from ocr.scripts import Script
+from one_barangay.local_settings import logger
 from one_barangay.scripts.service_account import firestore_auth
 
 firestore_app = firestore_auth("ocr_app")
@@ -48,3 +50,36 @@ class FirestoreModel:
         """
         doc_ref_house_num = self.doc_ref_rbi.document(str(data["house_num"]))
         doc_ref_house_num.set(data, merge=True)
+
+    def get_resident_rbi(self, user_data) -> dict:
+        """Get the residents RBI.
+
+        Args:
+          user_data: The data of the user to search for.
+
+        Returns:
+          The data dictionary of the user.
+        """
+        try:
+            doc = (
+                self.doc_ref_rbi.where(
+                    f"family_members.{user_data['first_name']}.last_name",
+                    "==",
+                    user_data["last_name"],
+                )
+                .limit(1)
+                .get()[0]
+            ).to_dict()
+            resident_data = doc["family_members"][user_data["first_name"]]
+            resident_data["address"] = doc["address"]
+
+            return resident_data
+        except IndexError as e:
+            logger.exception("User not found. %s", e)
+            return {}
+        except NotFound as e:
+            logger.exception("User not found. %s", e)
+            return {}
+        except TypeError as e:
+            logger.exception("Something went wrong. %s", e)
+            raise TypeError(f"Something went wrong. {e}") from e
