19bc71227d9d3aca32060312627ed91a6fc7fed3
PrynsTag <princevelasco16@gmail.com>
2021-10-31 17:41:15 +0800

    refactor[base.js]: make getCokkie and sendToServer global

--- one_barangay/static/assets/js/base.js
+++ one_barangay/static/assets/js/base.js
@@ -24,6 +24,39 @@ function showError(error) {
     });
 }
 
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i += 1) {
+            const cookie = cookies[i].trim();
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (`${name}=`)) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function sendToServer(url, msg) {
+    $.ajax({
+        url,
+        type: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-CSRFToken': getCookie('csrftoken'),
+        },
+        success: () => {
+            showSuccess(msg);
+        },
+        error: (errorThrown) => {
+            showError(errorThrown);
+        },
+    });
+}
+
 $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip()
 })
