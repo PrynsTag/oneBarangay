02ad986dac7cdbd4382c5e2a191cc882c54b0ecb
CWG.Ga <201812027@fit.edu.ph>
2021-11-17 21:31:34 +0800

    refactor: update appointment url path

--- appointment/urls.py
+++ appointment/urls.py
@@ -6,5 +6,61 @@ from . import views
 app_name = "appointment"
 
 urlpatterns = [
-    path("view-appointment", views.view_appointment, name="view-appointment")
+    path("document/request/", views.document_request, name="document_request"),
+    path(
+        "user/verification/<str:document_request_id>",
+        views.user_document_verification,
+        name="user_document_verification",
+    ),
+    path(
+        "user/document/cancel/",
+        views.user_document_cancel,
+        name="document_cancel",
+    ),
+    path(
+        "user/document/cancel/<str:document_request_id>",
+        views.user_document_cancel,
+        name="document_cancel",
+    ),
+    path(
+        "user/dummy/appointment/<int:account_num>",
+        views.add_document_request,
+        name="add_document_request",
+    ),
+    path(
+        "user/document/request/verified/",
+        views.user_document_verification,
+        name="document_request_verified",
+    ),
+    path(
+        "user/document/request/verified/<str:document_request_id>/<str:user_id>",
+        views.document_request_verified,
+        name="document_request_verified",
+    ),
+    path(
+        "user/document/issuing/list/<str:document_request_id>/<str:user_id>",
+        views.user_issuing_list,
+        name="user_issuing_list",
+    ),
+    path(
+        "user/document/issuing/success/<str:document_id>/<str:document_slugify>",
+        views.document_issuing_success,
+        name="document_issuing_success",
+    ),
+    path(
+        "user/document/issuing/process/<str:document_id>/<str:document_slugify>",
+        views.document_issuing_process,
+        name="document_issuing_process",
+    ),
+    path(
+        "user/document/request/verification/<str:document_request_id>",
+        views.user_verification_dt,
+        name="user_verification_dt",
+    ),
+    path(
+        "user/verification/selection/<str:document_request_id>",
+        views.user_selection_data,
+        name="user_selection_data",
+    ),
+    path("user/verification/selection/filter", views.user_filter, name="user_filter"),
 ]
