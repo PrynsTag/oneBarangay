633fc922365285b5f5f110c99fe6317d54302982
CWG.Ga <201812027@fit.edu.ph>
2021-11-18 12:30:33 +0800

    refactor: adde crispy forms bootstrap 5 in settings

--- one_barangay/settings.py
+++ one_barangay/settings.py
@@ -79,6 +79,14 @@ INSTALLED_APPS = [
     "ckeditor_uploader",
     "appointment",
     "services",
+    "crispy_forms",
+    "crispy_bootstrap5",
+]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 ]
 
 MIDDLEWARE = [
