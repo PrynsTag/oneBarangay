03004f24276b684d605acc6c2c5eb2fff4551d87
CWG.Ga <201812027@fit.edu.ph>
2021-11-20 19:52:49 +0800

    refactor: added barangay local employment form

--- appointment/forms.py
+++ appointment/forms.py
@@ -74,3 +74,21 @@ class BarangayIndigency(forms.Form):
     region = forms.CharField(label="Region", required=True)
     valid = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
     prepared = forms.CharField(label="Prepared by", required=True)
+
+
+class BarangayLocalEmployment(forms.Form):
+    """Class for Barangay Local Employment."""
+
+    date = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
+    fullname = forms.CharField(label="Fullname", max_length=100, min_length=2, required=True)
+    address = forms.CharField(label="Address", max_length=200, required=True)
+    year = forms.CharField(
+        label="Enter year of residency", max_length=4, min_length=4, required=True
+    )
+    conforme = forms.CharField(label="Conforme", required=True)
+    ctc = forms.CharField(label="CTC No.", max_length=15, required=True)
+    region = forms.CharField(label="Region", required=True)
+    orno = forms.CharField(label="OR No.", required=True)
+    amount = forms.DecimalField(label="Amount", decimal_places=2, required=True)
+    valid = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
+    prepared = forms.CharField(label="Prepared by", required=True)
