e40fb9649d590eff1e1324846ea02b41b22a66e8
CWG.Ga <201812027@fit.edu.ph>
2021-11-20 19:52:31 +0800

    refactor: added barangay indigency form

--- appointment/forms.py
+++ appointment/forms.py
@@ -56,3 +56,21 @@ class BarangayClearance(forms.Form):
     amount = forms.DecimalField(label="Amount", decimal_places=2, required=True)
     valid = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
     prepared = forms.CharField(label="Prepared by", required=True)
+
+
+class BarangayIndigency(forms.Form):
+    """Class for Barangay Indigency."""
+
+    date = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
+    fullname = forms.CharField(label="Fullname", max_length=100, min_length=2, required=True)
+    address = forms.CharField(label="Address", max_length=200, required=True)
+    year = forms.CharField(
+        label="Enter year of residency", max_length=4, min_length=4, required=True
+    )
+    purpose = forms.CharField(label="Purpose", required=True)
+    recipient = forms.CharField(label="Recipient", required=True)
+    conforme = forms.CharField(label="Conforme", required=True)
+    ctc = forms.CharField(label="CTC No.", max_length=15, required=True)
+    region = forms.CharField(label="Region", required=True)
+    valid = fields.DateField(widget=forms.widgets.DateInput(attrs={"type": "date"}))
+    prepared = forms.CharField(label="Prepared by", required=True)
